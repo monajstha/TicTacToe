9b157087fab9114526e57c0d42d445db688dee5e
monajstha <manojstha700@gmail.com>
2024-08-07 16:37:46 +0545

    add: add token add function

--- index.js
+++ index.js
@@ -6,7 +6,55 @@ function GameBoard() {
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
-      board[i].push("");
+      board[i].push(Cell());
     }
   }
+
+  const getBoard = () => board;
+
+  const markToken = (row, column, player) => {
+    if (board[row][column].getValue() !== "") {
+      alert("Please select an empty box!");
+    } else {
+      console.log({ row, column, player });
+      board[row][column].addToken(player);
+      console.log(board[row][column].getValue());
+    }
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
+
+  return {
+    getBoard,
+    markToken,
+    printBoard,
+  };
 }
+
+function Cell() {
+  let value = "";
+
+  const addToken = (player) => {
+    console.log({ player });
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue,
+  };
+}
+
+const gameboard = GameBoard();
+gameboard.markToken(0, 2, {
+  name: "Manoj",
+  token: "X",
+});
+gameboard.printBoard();
